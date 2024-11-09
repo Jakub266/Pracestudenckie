928e5b773ff73b2a9d0edd5ccccae8c4a6abb700
Jakub Brzezowski <kubabrzezowski@o2.pl>
2024-11-09 14:22:53 +0100

    dodanie znaku

--- dochód.java
+++ dochód.java
@@ -6,12 +6,12 @@ public class Main {
         System.out.println("Podaj wysokość twoich zarobków:");
         double many = input.nextInt();
 
-            if (many > 85.528){
+            if (many >= 85.528){
                 double procenty = (many + 18) / 100;
                 double y = many - procenty - 556.02;
                 System.out.println(y);
             }
-            else if (many < 85.528) {
+            else if (many <= 85.528) {
                 double procentx = (many + 32) / 100;
                 double x = many - 14.839 + procentx;
                 System.out.println(x);
